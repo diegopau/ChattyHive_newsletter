e677f9a8ccde8f66621e143cf34796857ec0a61a
Diego Pau <diegoocampo8@gmail.com>
2013-03-24 09:13:24 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChattyHiveNewsletter::Application.config.secret_token = '24e553f49962d0152feb739bc9bfd25bc2ac734bfcde768dd2cea4387774bc3d2c5ea806c90589388aecabfe40054d76357a1d7fd56ef97113d08e9263ef3df4'
